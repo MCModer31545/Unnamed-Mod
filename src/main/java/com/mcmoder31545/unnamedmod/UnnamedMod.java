8b484eabc2d95f0e65048b7eb1fafb7e76d5ff48
MCModer31545 <MCModerPL@gmail.com>
2014-07-22 22:24:35 +0200

    Created BaseMod
    Signed-off-by: MCModer31545 <MCModerPL@gmail.com>

---
+++ src/main/java/com/mcmoder31545/unnamedmod/UnnamedMod.java
@@ -0,0 +1,33 @@
+package com.mcmoder31545.unnamedmod;
+
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod (modid = "UnnamedMod", name = "Unnamed Mod", version = "1.7.10-1.0")
+
+public class UnnamedMod
+{
+    @Mod.Instance("UnnamedMod")
+    public static UnnamedMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
